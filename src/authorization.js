41827693c45c93fb1aa00e0200f25be8bbc5c11a
jayyxiao <123917335+jayyxiao@users.noreply.github.com>
2023-12-20 15:04:59 -0500

    Update authorization.js

--- src/authorization.js
+++ src/authorization.js
@@ -4,11 +4,12 @@ export async function redirectToAuthCodeFlow(clientId) {
     const challenge = await generateCodeChallenge(verifier);
 
     localStorage.setItem("verifier", verifier);
+    console.log(window.location.hostname);
 
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:3000/callback");
+    params.append("redirect_uri", "http://localhost:5173/callback");
     params.append("scope", "user-read-private user-read-email");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -24,7 +25,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:3000/callback");
+    params.append("redirect_uri", "http://localhost:5173/callback");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
